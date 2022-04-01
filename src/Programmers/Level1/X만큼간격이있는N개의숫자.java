06af2fc48b78b4bf7052183b80c52f96c5565d9b
dajeong <dajeongdev@gmail.com>
2022-04-01 17:38:21 +0900

    X만큼 간격이 있는 N개의 숫자

---
+++ src/Programmers/Level1/X만큼간격이있는N개의숫자.java
@@ -0,0 +1,25 @@
+package Programmers.Level1;
+
+public class X만큼간격이있는N개의숫자 {
+    /**
+     * [문제 설명]
+     * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+     * [제한 조건]
+     * x는 -10000000 이상, 10000000 이하인 정수입니다.
+     * n은 1000 이하인 자연수입니다.
+     */
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+        return answer;
+    }
+
+    public static void main(String ars[]) {
+        X만큼간격이있는N개의숫자.solution(-4, 2);
+    }
+}
