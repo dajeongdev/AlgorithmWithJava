852330f6f1789a0ac917c7d2117adc4ac98fd8a5
dajeongdev <dajeongdev@gmail.com>
2022-05-28 23:21:23 +0900

    프로그래머스 2문제, 백준 11문제

---
+++ src/Baekjoon/Level3/Problem2741.java
@@ -0,0 +1,36 @@
+package Baekjoon.Level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2741 {
+    /**
+     * [N 찍기]
+     * 문제
+     * 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+     *
+     * 입력
+     * 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+     *
+     * 출력
+     * 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+     *
+     * 예제 입력 1
+     * 5
+     * 예제 출력 1
+     * 1
+     * 2
+     * 3
+     * 4
+     * 5
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
