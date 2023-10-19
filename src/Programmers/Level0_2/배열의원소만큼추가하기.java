0921dee37116ae16f9dd5dc4a8694a8bcee49cf8
dajeongdev <dajeongdev@gmail.com>
2023-10-19 15:58:44 +0900

    프로그래머스 - 세 개의 구분자, 배열의 원소만큼 추가하기

---
+++ src/Programmers/Level0_2/배열의원소만큼추가하기.java
@@ -0,0 +1,78 @@
+package Programmers.Level0_2;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class 배열의원소만큼추가하기 {
+    /**
+     * 문제 설명
+     * 아무 원소도 들어있지 않은 빈 배열 X가 있습니다.
+     * 양의 정수 배열 arr가 매개변수로 주어질 때,
+     * arr의 앞에서부터 차례대로 원소를 보면서
+     * 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
+     *
+     * 제한사항
+     * 1 ≤ arr의 길이 ≤ 100
+     * 1 ≤ arr의 원소 ≤ 100
+     *
+     * 입출력 예
+     * arr	result
+     * [5, 1, 4]	[5, 5, 5, 5, 5, 1, 4, 4, 4, 4]
+     * [6, 6]	[6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+     * [1]	[1]
+     *
+     * 입출력 예 설명
+     * 입출력 예 #1
+     * 예제 1번에 대해서 a와 X를 나타내보면 다음 표와 같습니다.
+     * a	X
+     * []
+     * 5	[5, 5, 5, 5, 5]
+     * 1	[5, 5, 5, 5, 5, 1]
+     * 4	[5, 5, 5, 5, 5, 1, 4, 4, 4, 4]
+     * 따라서 [5, 5, 5, 5, 5, 1, 4, 4, 4, 4]를 return 합니다.
+     * 입출력 예 #2
+     * 예제 2번에 대해서 a와 X를 나타내보면 다음 표와 같습니다.
+     * a	X
+     * []
+     * 6	[6, 6, 6, 6, 6, 6]
+     * 6	[6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+     * 따라서 [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]를 return 합니다.
+     * 입출력 예 #3
+     * 예제 2번에 대해서 a와 X를 나타내보면 다음 표와 같습니다.
+     * a	X
+     * []
+     * 1	[1]
+     * 따라서 [1]을 return 합니다.
+     */
+    public int[] solution(int[] arr) {
+        int[] answer = new int[Arrays.stream(arr).sum()];
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            for (int k = j; k < j + arr[i]; k++)
+                answer[k] = arr[i];
+            j += arr[i];
+        }
+        return answer;
+    }
+
+    public int[] otherSolution(int[] arr) {
+        String answer = "";
+        for (int num : arr)
+            answer += (num + ",").repeat(num);
+        return Stream.of(answer.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static void main(String[] args) {
+        배열의원소만큼추가하기 T = new 배열의원소만큼추가하기();
+
+        int[] arr1 = {5, 1, 4};
+        int[] arr2 = {6, 6};
+        int[] arr3 = {1};
+
+        System.out.println(Arrays.toString(T.solution(arr1)));
+        System.out.println(Arrays.toString(T.solution(arr2)));
+        System.out.println(Arrays.toString(T.solution(arr3)));
+        System.out.println(Arrays.toString(T.otherSolution(arr1)));
+        System.out.println(Arrays.toString(T.otherSolution(arr2)));
+        System.out.println(Arrays.toString(T.otherSolution(arr3)));
+    }
+}
